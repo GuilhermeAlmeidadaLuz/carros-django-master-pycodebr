425f3eff819ab834378f44e9f2d6c55b1e11e95e
Guilherme Almeida da Luz <guilhermealmeidadaluz5@gmail.com>
2026-07-05 15:40:32 -0400

    feat(cars-admin): add CarAdmin config class to view on admin page - [Module 07 Lesson 037]

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+		# configurações do admin
+		list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+		search_fields = ('model',)
+		
+# agora é preciso indicar que a classe CarAdmin vai estar visível para o admin 
+# através da função register e incluir os models Car e CarAdmin
+admin.site.register(Car, CarAdmin) # parâmetros: (qual modelo?, quais configurações de admin?)
