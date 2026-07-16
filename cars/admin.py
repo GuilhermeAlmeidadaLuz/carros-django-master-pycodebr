9ed33b944625e3ddd56ab9c36475c48fca2b5979
Guilherme Almeida da Luz <guilhermealmeidadaluz5@gmail.com>
2026-07-16 17:15:32 -0400

    feat(alterations-in-models-app-settings-and-urls-routes): add codes from lessons [Module 07 Lesson 039 to Module 08 Lesson 043]

--- cars/admin.py
+++ cars/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
+
+class BrandAdmin(admin.ModelAdmin):
+		list_display = ('name',)
+		search_fields = ('name',)
 
 # Register your models here.
 class CarAdmin(admin.ModelAdmin):
@@ -9,4 +13,5 @@ class CarAdmin(admin.ModelAdmin):
 		
 # agora é preciso indicar que a classe CarAdmin vai estar visível para o admin 
 # através da função register e incluir os models Car e CarAdmin
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin) # parâmetros: (qual modelo?, quais configurações de admin?)
--- cars/models.py
+++ cars/models.py
@@ -3,13 +3,22 @@ from django.db import models
 # onde é definido o nome da tabela e campos (colunas)
 # dentro de django.db tem um módulo de nome 'models' com arquivo __init__.py, do qual a classe Car herda de models.Model
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):    # o nome da classe é o nome da tabela no banco
     id = models.AutoField(primary_key=True)     # chave única do tipo AutoField - autoincremental
     model = models.CharField(max_length=200)    # tipo CharField - string, outros parâmetros possíveis: blank=True, null=True
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') # <-- ajuste feito aqui para referenciar outro model criado (ou tabela no banco de dados)
     factory_year = models.IntegerField(blank=True, null=True)   # tipo IntegerField
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self): # <-- altera como um objeto da classe (instância) é exibido
         return self.model
