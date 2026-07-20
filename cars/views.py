7912c606bc42793b7bd40fe7152cdfae965bf6fb
Guilherme Almeida da Luz <guilhermealmeidadaluz5@gmail.com>
2026-07-20 12:30:59 -0400

    feat(cars-view): introduce a formatted response to route /cars [Module 08 Lesson 044]

--- cars/views.py
+++ cars/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cars_view(request):
-    return HttpResponse('Meus Carros 1')
+    html = '''
+        <html>
+            <head>
+                <title>Meus Carros</title>
+            </head>
+            <body>
+                <h1>Carros da PycodeBR</h1>
+                <h3>Só carro top!</h3>
+            </body>
+        </html>    
+    '''
+    return HttpResponse(html)
